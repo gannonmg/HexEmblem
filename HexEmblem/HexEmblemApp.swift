5ccb2e655217a5bad331c9fdbbbab863e8350d04
Matt Gannon <gannonm3211@gmail.com>
2026-08-06 19:16:32 -0600

    Initial Commit

---
+++ HexEmblem/HexEmblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexEmblemApp.swift
+//  HexEmblem
+//
+//  Created by Matt Gannon on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HexEmblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HexEmblemTests/HexEmblemTests.swift
@@ -0,0 +1,18 @@
+//
+//  HexEmblemTests.swift
+//  HexEmblemTests
+//
+//  Created by Matt Gannon on 8/6/26.
+//
+
+import Testing
+
+struct HexEmblemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
